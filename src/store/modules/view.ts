1156ffc82dd29cf032ff6b6ed5d58d16cbc4e5a8
seemrcola <seemrcola@163.com>
2023-03-04 10:43:57 +0800

    fix: 类型fix

--- src/store/modules/view.ts
+++ src/store/modules/view.ts
@@ -18,16 +18,16 @@ export interface IComponent {
 export const useViewStore = defineStore('view', () => {
   const components = ref<IComponent[]>([])
 
-  function addComponent(component: IComponent) {
+  function addComponent<T extends IComponent>(component: T) {
     components.value.push(markRaw(component))
   }
 
-  function removeComponent(component: IComponent) {
+  function removeComponent<T extends IComponent>(component: T) {
     const index = components.value.findIndex(comp => comp.id === component.id)
     components.value.splice(index, 1)
   }
 
-  function setComponentStyle(component: IComponent) {
+  function setComponentStyle<T extends IComponent>(component: T) {
     return {
       position: 'absolute',
       left: `${component.x}px`,
