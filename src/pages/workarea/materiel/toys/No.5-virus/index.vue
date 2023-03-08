6bba09c3f3d5eb70f9da745154b7100fc05e00cf
seemrcola <seemrcola@163.com>
2023-03-08 11:51:33 +0800

    feat: 疫情元胞自动机增加清零天数

--- src/pages/workarea/materiel/toys/No.5-virus/index.vue
+++ src/pages/workarea/materiel/toys/No.5-virus/index.vue
@@ -9,22 +9,25 @@ b.在每个时间步长中，模型会遍历所有元胞，并根据其周围八
 c.如果一个易感者的周围有感染者，则该易感者将以概率P被感染，概率P由传染率决定。
 d.如果一个感染者已经感染了T个时间步长，那么他就会变为康复者。
 e.康复者将保持免疫状态，不会再次感染。
-f.如果一个康复者周围有感染者，那么他将以概率Q失去免疫力。
+f.如果一个康复者周围有感染者，那么他将以概率Q失去免疫力，每感染一次，失去免疫力几率减半。
 g.如果一个感染者周围没有任何易感者，则他将在下一时间步长自动康复。
 h.当所有感染者都康复时，模型停止。
  -->
 
 <script setup lang='ts'>
-import { reactive } from 'vue'
+import { reactive, ref } from 'vue'
 import { CellStatus } from './type'
 import type { Person } from './type'
 
 const HEIGHT = 40
 const WIDTH = 40
 
-const P = 0.5 // 周围每一个感染者均50%几率传染给未感染过的元胞
-const T = 7 // 感染者7天后恢复正常
-const Q = 0.1 // 康复者周围每有感染者，有10%概率失去免疫力，每感染一次，失去免疫力几率减半
+const P = 0.7 // 周围每一个感染者均70%几率传染给未感染过的元胞-奥米克戎
+const T = 10 // 感染者10天后恢复正常
+const Q = 0.1 // 康复者周围每有感染者，有10%概率失去免疫力。
+
+let frameCounter = 0
+const day = ref(0)
 
 const state = reactive(
   Array.from(
@@ -128,13 +131,30 @@ function dueToD(cell: Person) {
   }
 }
 
-let frameCounter = 0
+// 是否清零
+function ifZero() {
+  for (const list of state) {
+    for (const cell of list) {
+      if (cell.status === CellStatus.V)
+        return false
+    }
+  }
+  return true
+}
+
 function startFrame() {
   requestAnimationFrame(() => {
     frameCounter++
     if (frameCounter % 20 === 0) {
       frameCounter = 0
-      start()
+      // 全部清零则停止
+      if (ifZero()) {
+        console.log('疫情结束')
+      }
+      else {
+        start()
+        day.value++
+      }
     }
     startFrame()
   })
@@ -143,17 +163,22 @@ startFrame()
 </script>
 
 <template>
-  <div w-100 h-100 f-c-c border="1px solid #fff" bg="yellow">
-    <div v-for="(list, index) of state" :key="index">
-      <div v-for="(cell, idx) of list" :key="idx" flex>
-        <div
-          border="1px solid #fff" m-0.25 rounded-0.2 w-2 h-2
-          :class="{
-            R: cell.status === CellStatus.R,
-            D: cell.status === CellStatus.D,
-            V: cell.status === CellStatus.V,
-          }"
-        />
+  <div w-100>
+    <div w-full h-6 f-c-c text-sm bg="#000" color="#fff" rounded-1>
+      清零天数 {{ day }}
+    </div>
+    <div w-full h-100 f-c-c border="1px solid #fff" bg="yellow">
+      <div v-for="(list, index) of state" :key="index">
+        <div v-for="(cell, idx) of list" :key="idx" flex>
+          <div
+            border="1px solid #fff" m-0.25 rounded-0.2 w-2 h-2
+            :class="{
+              R: cell.status === CellStatus.R,
+              D: cell.status === CellStatus.D,
+              V: cell.status === CellStatus.V,
+            }"
+          />
+        </div>
       </div>
     </div>
   </div>
