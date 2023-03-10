c4a55902006810436156788b22b6f165a9b309b1
seemrcola <seemrcola@163.com>
2023-03-10 15:33:59 +0800

    feat: seeting基础展示

--- src/pages/workarea/settings/index.vue
+++ src/pages/workarea/settings/index.vue
@@ -1,14 +1,15 @@
 <script setup lang='ts'>
 import { useViewStore } from '@/store/modules/view'
-const viewStore = useViewStore()
+const viewStore = (useViewStore())
 </script>
 
 <template>
   <div w-full h-full f-c-c>
     <div>
-      <div>x:{{ viewStore.taregtSelect?.x }} </div>
-      <div>y:{{ viewStore.taregtSelect?.y }} </div>
-      <div>r:{{ viewStore.taregtSelect?.rotate }} </div>
+      <div>x: {{ viewStore.taregtSelect?.x }} </div>
+      <div>y: {{ viewStore.taregtSelect?.y }} </div>
+      <div>rotate: {{ viewStore.taregtSelect?.rotate }} </div>
+      <div>scale: {{ viewStore.taregtSelect?.scale }} </div>
     </div>
   </div>
 </template>
