c613c25c50fa92e0be2d5e2a0e9d139750319b9b
seemrcola <seemrcola@163.com>
2023-03-18 22:03:30 +0800

    feat: random组件更改背景色合字体色

--- src/pages/workarea/materiel/toys/No.3-random/index.vue
+++ src/pages/workarea/materiel/toys/No.3-random/index.vue
@@ -4,7 +4,7 @@ const random = Math.random().toFixed(8)
 
 <template>
   <div
-    h="200px" w="200px"
+    h="200px" w="200px" bg="#eee" color="#000"
     f-c-c text-xl border="1px #fff solid"
   >
     {{ random }}
