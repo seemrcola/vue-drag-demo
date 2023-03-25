9eb697ad89b8d5b6bc08213132df0ba841aab789
seemrcola <seemrcola@163.com>
2023-03-25 23:41:23 +0800

    fix: 增加注释 组件名称变为自动导入

--- src/pages/workarea/materiel/toys/comp.config.ts
+++ src/pages/workarea/materiel/toys/comp.config.ts
@@ -5,17 +5,17 @@ interface ToysComponent extends IComponent {}
 export const toysComponentsConfig: ToysComponent[] = [
   // 元胞自动机-生命游戏
   {
-    type: compType.TOYS,
-    name: 'automaton',
-    id: '',
-    height: 400,
-    width: 400,
-    x: 0,
-    y: 0,
-    rotate: 0,
-    scale: [1, 1],
-    selecto: false,
-    component: `${compType.TOYS}automaton`,
+    type: compType.TOYS, // 组件类型
+    name: 'automaton', // 组件名称
+    id: '', // 组件id
+    height: 400, // 组件高度
+    width: 400, // 组件宽度
+    x: 0, // 相对画布的x距离
+    y: 0, // 相对画布的y距离
+    rotate: 0, // 旋转角度
+    scale: [1, 1], // 缩放
+    selecto: false, // 是否选中
+    component: `${compType.TOYS}automaton`, // 为防止名称重复 组件注册时候的名称就用类型+名称来表示
   },
   // mines扫雷
   {
--- src/pages/workarea/materiel/toys/comp.index.ts
+++ src/pages/workarea/materiel/toys/comp.index.ts
@@ -1,13 +1,8 @@
 import { defineAsyncComponent } from 'vue'
+import { toysComponentsConfig } from './comp.config'
 import { compType } from '@/enum/materiel.enum'
 
-const components = [
-  'mines',
-  'plum',
-  'random',
-  'automaton',
-  'virus',
-]
+const components = toysComponentsConfig.map(config => config.name)
 
 export function setupToysComponents(app: any) {
   components.forEach((name) => {
