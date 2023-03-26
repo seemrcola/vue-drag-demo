1184dde014276287a28d5624b1e52c1f3fdb7851
seemrcola <seemrcola@163.com>
2023-03-26 14:07:40 +0800

    test: 03

--- src/pages/workarea/materiel/toys/comp.index.ts
+++ src/pages/workarea/materiel/toys/comp.index.ts
@@ -5,9 +5,9 @@ import { compType } from '@/enum/materiel.enum'
 const components = toysComponentsConfig.map(config => config.name)
 
 export function setupToysComponents(app: any) {
-  components.forEach((name) => {
+  components.forEach(async (name) => {
     const instance = defineAsyncComponent(() => import(`./${name}/index.vue`))
     console.log('000000')
-    app.component(`${compType.TOYS}${name}`, instance)
+    await app.component(`${compType.TOYS}${name}`, instance)
   })
 }
