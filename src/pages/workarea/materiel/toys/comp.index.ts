8356819cef9e739fb929aa4f03d67226a3b4c079
seemrcola <seemrcola@163.com>
2023-03-26 19:07:45 +0800

    test: 08

--- src/pages/workarea/materiel/toys/comp.index.ts
+++ src/pages/workarea/materiel/toys/comp.index.ts
@@ -1,15 +1,29 @@
-import { defineAsyncComponent } from 'vue'
-import { toysComponentsConfig } from './comp.config'
-import { compType } from '@/enum/materiel.enum'
+// import { defineAsyncComponent } from 'vue'
+// import { toysComponentsConfig } from './comp.config'
+// import { compType } from '@/enum/materiel.enum'
 
-const components = toysComponentsConfig.map(config => config.name)
+// const components = toysComponentsConfig.map(config => config.name)
+
+// export function setupToysComponents(app: any) {
+//   components.forEach((name) => {
+//     const instance = defineAsyncComponent({
+//       loader: () => import(`./${name}/index.vue`),
+//       onError: () => { console.log('[Error] render failed') },
+//     })
+//     app.component(`${compType.TOYS}-${name}`, instance)
+//   })
+// }
+
+import Virus from './virus/index.vue'
+import Random from './random/index.vue'
+import Plum from './plum/index.vue'
+import Mines from './mines/index.vue'
+import Automaton from './automaton/index.vue'
 
 export function setupToysComponents(app: any) {
-  components.forEach((name) => {
-    const instance = defineAsyncComponent({
-      loader: () => import(`./${name}/index.vue`),
-      onError: () => { console.log('[Error] render failed') },
-    })
-    app.component(`${compType.TOYS}-${name}`, instance)
-  })
+  app.component('ToysVirus', Virus)
+  app.component('ToysRandom', Random)
+  app.component('ToysPlum', Plum)
+  app.component('ToysMines', Mines)
+  app.component('ToysAutomaton', Automaton)
 }
