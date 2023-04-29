1bdf53fe00ee5adbd3ae8e71a5b6c405fc4726ef
seemrcola <seemrcola@163.com>
2023-04-29 23:18:14 +0800

    wip: 添加注释，并暂时停止处理resize的问题

--- src/pages/workarea/materiel/charts/sun/index.vue
+++ src/pages/workarea/materiel/charts/sun/index.vue
@@ -10,6 +10,7 @@ const main = ref<HTMLElement>()
 onMounted(() => {
   const chart = echarts.init(main.value!)
   chart.setOption(option)
+  // todo: 这个reszie其实不会成功 因为设计上我改变的是transform 而不是宽高 这个时候reszie会静默执行失败
   useChartResize(chart)
 })
 </script>
